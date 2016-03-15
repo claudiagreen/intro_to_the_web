735601120a8980a6d46dd9af0bad7d119035942a
Claudia Green <claudia@claudiag.me.uk>
2016-03-15 10:33:19 +0000

    Worked up to challenge 7.

--- app.rb
+++ app.rb
@@ -1,1 +1,18 @@
 require 'sinatra'
+set :session_secret, 'super secret'
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "This is a secret."
+end
+
+get '/carrot' do
+  "I love carrot."
+end
+
+get '/cat' do
+  '<img src="http://bit.ly/1eze8aE" style="border: 2px dashed black"/>' 
+end
